efbbdb30b729dbf153a075707955f3a48548d88e
Jaqueline Cantarino <jaquelinecantarinoc@gmail.com>
2023-01-10 21:45:51 -0300

    Exercicio Threads, ordem de exibição

---
+++ OrdemExcecucaoThreads/src/ImprimeNumero.java
@@ -0,0 +1,22 @@
+
+public class ImprimeNumero implements Runnable{
+
+	@Override
+	public void run() {
+		
+		int numero = 0;
+		
+		while(numero <= 1000) { //enquanto o meu numero for menor ou igual a 1000 vai executar os comandos abaixo
+			System.out.println(numero); //imprime o numero na tela
+			
+			//esse espaço cria e imprime o id da thread, usamos para ver a ordem de execução paralela que é criada
+			Thread threadAtual = Thread.currentThread();
+			long id = threadAtual.getId();
+			System.out.println("Id da thread: " + id);
+			
+			numero++; //incrementa o numero a cada volta
+		}
+		
+	}
+
+}
---
+++ OrdemExcecucaoThreads/src/Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Thread threadUm = new Thread(new ImprimeNumero(), "threadUm"); //criando a primeira thread
+		Thread threadDois = new Thread(new ImprimeNumero(), "threadDois"); //criando a segunda thread
+
+		//iniciando
+		threadUm.start();
+		threadDois.start();
+		
+		//Objetivo: Analisar a ordem de execução que pode ser feita das threads, não existe uma regra
+
+	}
+
+}
