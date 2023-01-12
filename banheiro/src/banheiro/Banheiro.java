8f316d1060b8a239608d00cb08f3498546bbd103
Jaqueline Cantarino <jaquelinecantarinoc@gmail.com>
2023-01-11 21:43:53 -0300

    Implementando ReentrantLock nas nossas threads - alternativa para o synchronized

--- banheiro/src/banheiro/Banheiro.java
+++ banheiro/src/banheiro/Banheiro.java
@@ -1,39 +1,95 @@
 package banheiro;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Banheiro {
+	
+	//Nesses exemplos eu vou usar o synchronyzed em um bloco e o ReentrantLock em outro para ter exemplos visuais das duas alternativas
+	
+//	public void fazNumeroUm() {
+//		
+//		//Aqui criamos essa linha para que ele possa retornar o thread atual, ou seja, nessa linha ele vai exibir o nome criado lá na principal (ultimo parametro na instancia de thread) que identifica o thread
+//		String nome = Thread.currentThread().getName();
+//		
+//		//Essa ação vai ocorrer sem regra, em qualquer ordem
+//		System.out.println(nome + " batendo na porta!!!");
+//		
+//		//O synchronized faz com que as ações da thread não sejam executadas sem ordem, ou seja, tudo que está dentro desse trecho só é executado se outra ação da thread não tiver sendo executada
+//		//modificador que deve ser colocado em um método para que não possa ser executado por duas Threads ao mesmo tempo
+//		synchronized (this) { //essa chave(this) também é chamado de mutex
+//			System.out.println(nome + " Entrando no banheiro");
+//			System.out.println(nome + " Fazendo coisa rapida");
+//			try {
+//				Thread.sleep(5000);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			System.out.println(nome + " Dando Descarga");
+//			System.out.println(nome + " Lavando as mãos");
+//			System.out.println(nome + " Saindo do Banheiro");			
+//		}
+//	}
+//	
+//	public void fazNumeroDois() {
+//		
+//		String nome = Thread.currentThread().getName();
+//		
+//		System.out.println(nome + " batendo na porta!!!");
+//		
+//		synchronized (this) {
+//			System.out.println(nome + " Entrando no banheiro");
+//			System.out.println(nome + " Fazendo coisa demorada");
+//			try {
+//				Thread.sleep(10000);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			System.out.println(nome + " Dando Descarga");
+//			System.out.println(nome + " Lavando as mãos");
+//			System.out.println(nome + " Saindo do Banheiro");			
+//		}
+//	}
+	
+    private Lock lock = new ReentrantLock();
 	
 	public void fazNumeroUm() {
 		
-		//Aqui criamos essa linha para que ele possa retornar o thread atual, ou seja, nessa linha ele vai exibir o nome criado lá na principal (ultimo parametro na instancia de thread) que identifica o thread
 		String nome = Thread.currentThread().getName();
 		
-		//Essa ação vai ocorrer sem regra, em qualquer ordem
 		System.out.println(nome + " batendo na porta!!!");
 		
-		//O synchronized faz com que as ações da thread não sejam executadas sem ordem, ou seja, tudo que está dentro desse trecho só é executado se outra ação da thread não tiver sendo executada
-		synchronized (this) {
+		lock.lock(); //Explicitamente mostramos no código quando abre e quando fecha a porta da thread
+		
+		//todo o restante funciona igual
+		
 			System.out.println(nome + " Entrando no banheiro");
 			System.out.println(nome + " Fazendo coisa rapida");
+			
 			try {
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			
 			System.out.println(nome + " Dando Descarga");
 			System.out.println(nome + " Lavando as mãos");
-			System.out.println(nome + " Saindo do Banheiro");			
-		}
+			System.out.println(nome + " Saindo do Banheiro");
+			
+		lock.unlock(); // fechando a porta
 	}
-	
+    
 	public void fazNumeroDois() {
 		
 		String nome = Thread.currentThread().getName();
 		
 		System.out.println(nome + " batendo na porta!!!");
 		
-		synchronized (this) {
+		lock.lock();
+		
 			System.out.println(nome + " Entrando no banheiro");
 			System.out.println(nome + " Fazendo coisa demorada");
+			
 			try {
 				Thread.sleep(10000);
 			} catch (InterruptedException e) {
@@ -42,7 +98,7 @@ public class Banheiro {
 			System.out.println(nome + " Dando Descarga");
 			System.out.println(nome + " Lavando as mãos");
 			System.out.println(nome + " Saindo do Banheiro");
-		}
+		lock.unlock();
 	}
 
 }
--- banheiro/src/banheiro/Principal.java
+++ banheiro/src/banheiro/Principal.java
@@ -8,9 +8,14 @@ public class Principal {
 		
 		Thread convidadoUm = new Thread(new TarefaUm(banheiro),"ConvidadoUm");
 		Thread convidadoDois = new Thread(new TarefaDois(banheiro),"ConvidadoDois");
+		Thread convidadoTres = new Thread(new TarefaUm(banheiro),"ConvidadoTres");
+
 		
 		convidadoUm.start();
 		convidadoDois.start();
+		convidadoTres.start();
 	}
-
+	
+	//operação atômica= operações que a execução não pode ser interrompida na metade.
+//ReentrantLock = alternativa para o synchronized que faz o bloqueio de forma explícita no código
 }
